207767d0b674c4e964948b0ecfab933e9f295155
Frank Madrid II <fmadrid@asmodeena.com>
2020-05-14 20:47:14 -0500

    Remove signal that creates Tokens for new users

--- backend/api/models.py
+++ backend/api/models.py
@@ -32,15 +32,3 @@ class Membership(models.Model):
 
     class Meta:
         unique_together = ['person', 'group']
-
-
-from django.conf import settings
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from rest_framework.authtoken.models import Token
-
-
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
