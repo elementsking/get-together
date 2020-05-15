a00694e8d74b93dfbe7e550241d9269e0e17a347
Frank Madrid II <fmadrid@asmodeena.com>
2020-05-15 00:48:59 -0500

    serialize posts and members in Group

--- backend/api/serializers/get_together.py
+++ backend/api/serializers/get_together.py
@@ -1,11 +1,20 @@
-from api.models import Group, Membership
+from api.models import Group, Membership, Message
+from api.serializers import UserSerializer
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = '__all__'
 
 
 class GroupSerializer(serializers.ModelSerializer):
     name = serializers.CharField(
         validators=[UniqueValidator(queryset=Group.objects.all())])
+    posts = MessageSerializer(many=True, read_only=True)
+    members = UserSerializer(many=True, read_only=True)
 
     def validate_name(self, value):
         if ' ' in value:
@@ -14,7 +23,7 @@ class GroupSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Group
-        fields = ['id', 'name', 'members']
+        fields = ['id', 'name', 'members', 'posts']
 
 
 class MembershipSerializer(serializers.ModelSerializer):
